4567535f1a19e7c91e289baf69613669089aa324
czuellig <cedric.zuellig@gmail.com>
2024-06-21 14:27:25 +0200

    gallery mit grid und popup

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const gallery = document.getElementById('gallery');
 const popup = document.getElementById('popup');
 const selectedImage = document.getElementById('selectedImage');
-const imageIndexes = [1, 2, 3];
+const imageIndexes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const selectedIndex = null;
 
 imageIndexes.forEach(i => {
@@ -12,8 +12,16 @@ imageIndexes.forEach(i => {
 
    image.addEventListener('click', () => {
       // popup
+      popup.style.transform = 'translateY(0)';
+      selectedImage.src = `/images/bild-tag-${i}.jpg`;
+      selectedImage.alt = `Bild von Tag ${i}`;
    });
 
    gallery.appendChild(image);
+});
 
+popup.addEventListener('click', () => {
+   popup.style.transform = 'translateY(-100%)';
+   popup.src = '';
+   popup.alt = '';
 });
